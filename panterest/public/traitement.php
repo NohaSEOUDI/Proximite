144b69c7fc85e35a3f110104082af027b69702ba
Seoudi Noha <noha.seoudi@etu.umontpellier.fr>
2021-02-18 16:54:03 +0100

    add README

---
+++ panterest/public/traitement.php
@@ -0,0 +1,12 @@
+<?php
+	if(isset($_POST["s_inscrire"])){
+	if(isset($_POST["email"]) &&  isset($_POST["password"]) && isset($_POST["passwordC"])){
+			if($_POST["password"]==$_POST["passwordC"]){
+				
+			}
+		}
+	}
+	
+
+
+?>
