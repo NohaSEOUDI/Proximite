4d87a5fd0e70222ac3d0e2bf8703f05ea57ec9ea
Seoudi Noha <noha.seoudi@etu.umontpellier.fr>
2021-02-18 16:59:10 +0100

    add dossier panterest1

---
+++ panterest1/src/Controller/PinsController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\Pin;
+use App\Entity\User;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PinsController extends AbstractController
+{	private $em;
+
+    /**
+     * @Route("/pins",name="app_home",methods={"Get","POST"})
+     */
+    public function index(PinRepository $rep): Response
+    {	
+        /*$Auth=new Auth;
+       // $Auth->setId(1);
+        $Auth->setEmail('nnn@gamil.com');
+        $Auth->setMotDePass('123');
+        $Auth->setConfMotDePass('123');
+        $em=$this->getDoctrine()->getManager();
+        $em->persist($Auth);
+        $em->flush();*/
+      
+        return $this->render('pins/index.html.twig');
+    }
+     /**
+     * @Route("/pins/create",name="app_pins_create" ,methods={"Get","POST"})
+     */
+    public function create(Request $request,EntityManagerInterface $em): Response{
+        $form = $this->createFormBuilder()
+            ->add('email')
+           // ->add('pseudo')
+            ->add('password')
+            //->add('password2')
+           //->add('submit', SubmitType::class, ['label'=>'create Client account '])
+            ->getForm()
+        ;
+       // dd($form);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){//type post
+            //dd('Taitement');
+            $data=$form->getData();
+            $user=new User();
+          //  $user->setUuid("okjpk");//?
+           // $user->setRoles($data['role']);
+            $user->setPassword($data['password']);
+            $user->setEmail($data['email']);
+            //$user->setPassword2($data['password2']);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }   
+    	return $this->render('pins/create.html.twig',['form' => $form->createView()
+         ]);
+    }
+
+    /**
+     * @Route("/pins/inscription",name="app_pins_inscrit" ,methods={"Get","POST"})
+     */
+    public function inscrit(Request $request,EntityManagerInterface $em): Response{
+      /*$form = $this->createFormBuilder()
+            ->add('email')
+            ->add('pseudo')
+            ->add('password')
+            ->add('password2')
+           //->add('submit', SubmitType::class, ['label'=>'create Client account '])
+            ->getForm()
+        ;
+       // dd($form);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){//type post
+            //dd('Taitement');
+            $data=$form->getData();
+            $user=new User();
+            $user->setUuid("okjpk");//?
+           // $user->setRoles($data['role']);
+            $user->setPassword($data['password']);
+            $user->setEmail($data['email']);
+            $user->setPassword2($data['password2']);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }   */
+        return $this->render('pins/inscription.html.twig');
+    }
+}
